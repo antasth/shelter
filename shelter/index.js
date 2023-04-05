193beb85a941d3da629afec47977b386b9ca1002
Anton Astashov <antasth@mail.ru>
2023-04-05 12:21:57 +0300

    refactor: make burger menu fixed

--- shelter/index.js
+++ shelter/index.js
@@ -3,6 +3,7 @@ const burgerMenu = document.querySelector('.burger')
 
 burgerIcon.forEach((icon) => {
   icon.addEventListener('click', () => {
+    icon.classList.toggle('burger__icon-active')
     !burgerMenu.classList.contains('burger__active')
       ? burgerMenu.classList.add('burger__active')
       : burgerMenu.classList.remove('burger__active')
