e4062e5dcc71a65b7be3d23f0ab8252d9112f2c4
Anton Astashov <antasth@mail.ru>
2023-04-05 16:54:00 +0300

    feat: hide burger after click on burger link

--- shelter/index.js
+++ shelter/index.js
@@ -1,11 +1,17 @@
 const burgerIcon = document.querySelector('.burger__icon')
 const burgerMenu = document.querySelector('.burger')
+const burgerMenuItem = document.querySelectorAll('.burger__list-item')
 
+// show or hide burger menu and rotate icon
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('burger__icon-active')
-
-  !burgerMenu.classList.contains('burger__active')
-    ? burgerMenu.classList.add('burger__active')
-    : burgerMenu.classList.remove('burger__active')
+  burgerMenu.classList.toggle('burger__active')
 })
 
+// if click on burger link => hide burger menu
+burgerMenuItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger__active')
+    burgerIcon.classList.remove('burger__icon-active')
+  })
+})
--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -1,14 +1,23 @@
+const burgerIcon = document.querySelector('.burger__icon')
 const burgerIconBlack = document.querySelector('.burger__icon-black')
 const burgerMenu = document.querySelector('.burger')
+const burgerMenuItem = document.querySelectorAll('.burger__list-item')
 
+// show or hide burger menu and rotate icon
 burgerIconBlack.addEventListener('click', () => {
   burgerIconBlack.classList.toggle('burger__icon-active')
-
-  !burgerMenu.classList.contains('burger__active')
-    ? burgerMenu.classList.add('burger__active')
-    : burgerMenu.classList.remove('burger__active')
+  burgerMenu.classList.toggle('burger__active')
 })
-
+// change icon color
 burgerIconBlack.addEventListener('click', () => {
   burgerIconBlack.classList.toggle('burger__icon-black')
+})
+
+// if click on burger link => hide burger menu
+burgerMenuItem.forEach((item) => {
+  item.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger__active')
+    burgerIcon.classList.remove('burger__icon-active')
+    burgerIconBlack.classList.toggle('burger__icon-black')
+  })
 })
