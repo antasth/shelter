1dedc19e59bd4f20cc710224a491450c3ae88612
Anton Astashov <antasth@mail.ru>
2023-04-05 17:35:30 +0300

    feat: add background when burger is open

--- shelter/index.js
+++ shelter/index.js
@@ -2,12 +2,14 @@ const burgerIcon = document.querySelector('.burger__icon')
 const burgerMenu = document.querySelector('.burger')
 const burgerMenuItem = document.querySelectorAll('.burger__list-item')
 const body = document.querySelector('body')
+const burgerBackground = document.querySelector('.burger__background')
 
 // show or hide burger menu and rotate icon
 burgerIcon.addEventListener('click', () => {
   burgerIcon.classList.toggle('burger__icon-active')
   burgerMenu.classList.toggle('burger__active')
   body.classList.toggle('locked')
+  burgerBackground.classList.toggle('burger__background-active')
 })
 
 // if click on burger link => hide burger menu
@@ -16,5 +18,6 @@ burgerMenuItem.forEach((item) => {
     burgerMenu.classList.remove('burger__active')
     burgerIcon.classList.remove('burger__icon-active')
     body.classList.remove('locked')
+    burgerBackground.classList.remove('burger__background-active')
   })
 })
--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -3,12 +3,16 @@ const burgerIconBlack = document.querySelector('.burger__icon-black')
 const burgerMenu = document.querySelector('.burger')
 const burgerMenuItem = document.querySelectorAll('.burger__list-item')
 const body = document.querySelector('body')
+const burgerBackground = document.querySelector('.burger__background')
+
 
 // show or hide burger menu and rotate icon
 burgerIconBlack.addEventListener('click', () => {
   burgerIconBlack.classList.toggle('burger__icon-active')
   burgerMenu.classList.toggle('burger__active')
   body.classList.toggle('locked')
+  burgerBackground.classList.toggle('burger__background-active')
+
 })
 // change icon color
 burgerIconBlack.addEventListener('click', () => {
@@ -22,5 +26,7 @@ burgerMenuItem.forEach((item) => {
     burgerIcon.classList.remove('burger__icon-active')
     burgerIconBlack.classList.toggle('burger__icon-black')
     body.classList.remove('locked')
+    burgerBackground.classList.remove('burger__background-active')
+
   })
 })
