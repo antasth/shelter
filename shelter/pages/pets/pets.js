2c63f83c89fca56947d3d5d97275c45fa8aa4721
Anton Astashov <antasth@mail.ru>
2023-04-06 13:36:09 +0300

    refactor: burger in pets.js add functions hideBurger and toggleBurger

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -8,18 +8,7 @@ const body = document.querySelector('body')
 
 // show or hide burger menu and rotate icon
 burgerIconBlack.addEventListener('click', () => {
-  // if scroll is hidden => add margin right
-  let marginSize = window.innerWidth - document.documentElement.clientWidth
-  if (marginSize) {
-    document.documentElement.style.marginRight = marginSize + 'px'
-  } else {
-    document.documentElement.style.marginRight = 0
-  }
-
-  burgerIconBlack.classList.toggle('burger__icon-active')
-  burgerMenu.classList.toggle('burger__active')
-  body.classList.toggle('locked')
-  burgerBackground.classList.toggle('burger__background-active')
+  toggleBurger()
 })
 // change icon color
 burgerIconBlack.addEventListener('click', () => {
@@ -29,31 +18,39 @@ burgerIconBlack.addEventListener('click', () => {
 // if click on burger link => hide burger menu
 burgerMenuItem.forEach((item) => {
   item.addEventListener('click', () => {
-    body.classList.remove('locked')
-    burgerMenu.classList.remove('burger__active')
-    burgerIcon.classList.remove('burger__icon-active')
-    burgerIconBlack.classList.toggle('burger__icon-black')
-    burgerBackground.classList.remove('burger__background-active')
-    document.documentElement.style.marginRight = 0
+    hideBurger()
   })
 })
 // if click on burger background => hide burger menu
 burgerBackground.addEventListener('click', () => {
-  body.classList.toggle('locked')
-  burgerMenu.classList.toggle('burger__active')
-  burgerIconBlack.classList.toggle('burger__icon-black')
-  burgerIconBlack.classList.toggle('burger__icon-active')
-  burgerBackground.classList.toggle('burger__background-active')
-  document.documentElement.style.marginRight = 0
+  hideBurger()
 })
 
 window.addEventListener('resize', () => {
   if (document.body.clientWidth > 767) {
-    body.classList.remove('locked')
-    burgerMenu.classList.remove('burger__active')
-    burgerIcon.classList.remove('burger__icon-active')
-    burgerIconBlack.classList.add('burger__icon-black')
-    burgerBackground.classList.remove('burger__background-active')
-    document.documentElement.style.marginRight = 0
+    hideBurger()
   }
 })
+
+function toggleBurger() {
+  // if scroll is hidden => add margin right
+  let marginSize = window.innerWidth - document.documentElement.clientWidth
+  if (marginSize) {
+    document.documentElement.style.marginRight = marginSize + 'px'
+  } else {
+    document.documentElement.style.marginRight = 0
+  }
+  burgerIconBlack.classList.toggle('burger__icon-active')
+  burgerMenu.classList.toggle('burger__active')
+  body.classList.toggle('locked')
+  burgerBackground.classList.toggle('burger__background-active')
+}
+
+function hideBurger() {
+  body.classList.remove('locked')
+  burgerMenu.classList.remove('burger__active')
+  burgerBackground.classList.remove('burger__background-active')
+  burgerIcon.classList.remove('burger__icon-active')
+  burgerIconBlack.classList.add('burger__icon-black')
+  document.documentElement.style.marginRight = 0
+}
