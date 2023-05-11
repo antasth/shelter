c2d2a559925c6633ffcd3b0db8a27cd372c26610
Anton Astashov <antasth@mail.ru>
2023-05-11 23:16:10 +0300

    feat: add recursive getBombs function call when current cell is empty

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -2,6 +2,7 @@ const width = 15
 const height = 15
 const bombsCount = 50
 let boardSize = width * height
+const zeroCells = []
 
 function createBoard(size) {
   const board = document.createElement('div')
@@ -34,8 +35,9 @@ board.addEventListener('click', (e) => {
       e.target.innerHTML = 'B'
       e.target.classList.add('disabled')
     } else {
-      const bombsCount = getBombs(Number(e.target.id))
-      e.target.innerHTML = bombsCount
+      getBombs(Number(e.target.id))
+      // const bombsCount = getBombs(Number(e.target.id))
+      // e.target.innerHTML = bombsCount
     }
   }
 })
@@ -44,46 +46,76 @@ const getBombs = (cellId) => {
   const column = cellId % width !== 0 ? cellId % width : width
   const row = Math.ceil(cellId / width)
   let count = 0
+  let aroundCells = []
   if (row !== 1 && row !== width && column !== 1 && column !== width) {
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
-        if (bombs.includes((row + i - 1) * width + column + j)) {
+        let id = (row + i - 1) * width + column + j
+        if (bombs.includes(id)) {
           count++
+        } else {
+          aroundCells.push(id)
         }
       }
     }
   } else if (column === 1) {
     for (let i = -1; i <= 1; i++) {
       for (let j = 0; j <= 1; j++) {
-        if (bombs.includes((row + i - 1) * width + column + j)) {
+        let id = (row + i - 1) * width + column + j
+        if (bombs.includes(id)) {
           count++
+        } else {
+          aroundCells.push(id)
         }
       }
     }
   } else if (column === width) {
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 0; j++) {
-        if (bombs.includes((row + i - 1) * width + column + j)) {
+        let id = (row + i - 1) * width + column + j
+        if (bombs.includes(id)) {
           count++
+        } else {
+          aroundCells.push(id)
         }
       }
     }
   } else if (row === 1) {
     for (let i = 0; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
-        if (bombs.includes((row + i - 1) * width + column + j)) {
+        let id = (row + i - 1) * width + column + j
+        if (bombs.includes(id)) {
           count++
+        } else {
+          aroundCells.push(id)
         }
       }
     }
   } else if (row === width) {
     for (let i = -1; i <= 0; i++) {
       for (let j = -1; j <= 1; j++) {
-        if (bombs.includes((row + i - 1) * width + column + j)) {
+        let id = (row + i - 1) * width + column + j
+        if (bombs.includes(id)) {
           count++
+        } else {
+          aroundCells.push(id)
         }
       }
     }
   }
+
+  if (count === 0) {
+    zeroCells.push(cellId)
+    aroundCells = aroundCells.filter((cell) => cell !== cellId)
+    console.log(aroundCells)
+    aroundCells.forEach((cell) => {
+      if (!zeroCells.includes(cell) && cell > 0 && cell < boardSize) {
+        getBombs(cell)
+      }
+    })
+  }
+  const cell = document.getElementById(`${cellId}`)
+  cell.innerHTML = count === 0 ? '' : count
+  cell.classList.add('opened')
   return count
 }
