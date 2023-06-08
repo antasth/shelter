65d22688d829f02213750f40b2675ff6dfeb30a5
Anton Astashov <antasth@mail.ru>
2023-06-08 20:19:11 +0300

    feat: add apiKey to project

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '615f148c6f6d4d39868811b11876f3e9', // получите свой ключ https://newsapi.org/
         });
     }
 }
