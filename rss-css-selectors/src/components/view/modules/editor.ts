7e7f60f246db707572906a745195981bd54ad0e3
Anton Astashov <antasth@mail.ru>
2023-06-27 15:52:21 +0300

    refactor: class Editor using createElement function

--- rss-css-selectors/src/components/view/modules/editor.ts
+++ rss-css-selectors/src/components/view/modules/editor.ts
@@ -14,49 +14,27 @@ class Editor {
         editor.append(this.drawCssEditor(), this.drawHtmlEditor());
         this.createHtmlContent(this.level);
     }
-    private drawCssEditor(): HTMLDivElement {
-        const cssEditor = document.createElement('div');
-        cssEditor.classList.add('editor__css');
+    private drawCssEditor(): HTMLElement {
+        const cssEditor = createElement('div', 'editor__css', '', null);
+        createElement('div', 'editor__header', 'CSS', cssEditor);
+        const cssEditorContent = createElement('div', 'editor__content-css', '', cssEditor);
 
-        const cssEditorHeader = document.createElement('div');
-        cssEditorHeader.classList.add('editor__header');
-        cssEditorHeader.innerText = 'CSS';
-
-        const cssEditorContent = document.createElement('div');
-        cssEditorContent.classList.add('editor__content-css');
-
-        const listNumbers = createNumbersList();
-
-        const editorInput = document.createElement('input');
-        editorInput.classList.add('editor__input');
+        const inputContent = createElement('div', 'editor__input__content', '', cssEditorContent);
+        const editorInput = createElement('input', 'editor__input', '', inputContent);
         editorInput.setAttribute('placeholder', 'введите CSS селектор');
         editorInput.setAttribute('type', 'text');
-
-        const submitButton = document.createElement('button');
-        submitButton.classList.add('editor__button');
-        submitButton.textContent = 'Enter';
-        const inputContent = document.createElement('div');
-        inputContent.classList.add('editor__input__content');
-        inputContent.append(editorInput, submitButton);
-
-        cssEditorContent.append(listNumbers, inputContent);
-        cssEditor.append(cssEditorHeader, cssEditorContent);
-        return cssEditor;
-    }
-    private drawHtmlEditor(): HTMLDivElement {
-        const htmlEditor = document.createElement('div');
-        htmlEditor.classList.add('editor__html');
-
-        const cssEditorHeader = document.createElement('div');
-        cssEditorHeader.classList.add('editor__header');
-        cssEditorHeader.innerText = 'HTML';
-
-        const editorContent = document.createElement('div');
-        editorContent.classList.add('editor__content-html');
+        createElement('button', 'editor__button', 'Enter', inputContent);
 
         const listNumbers = createNumbersList();
+        cssEditorContent.append(listNumbers, inputContent);
+        return cssEditor;
+    }
+    private drawHtmlEditor(): HTMLElement {
+        const htmlEditor = createElement('div', 'editor__html', '', null);
+        createElement('div', 'editor__header', 'HTML', htmlEditor);
+        const editorContent = createElement('div', 'editor__content-html', '', htmlEditor);
+        const listNumbers = createNumbersList();
         editorContent.append(listNumbers);
-        htmlEditor.append(cssEditorHeader, editorContent);
         return htmlEditor;
     }
 
@@ -64,19 +42,17 @@ class Editor {
         this.data = levels[level].htmlContent;
         if (document.querySelector('.editor__content')) this.clearHtmlContent();
         const editorHtml = getElement('.editor__content-html');
-        const editorHtmlContent = document.createElement('div');
-        editorHtmlContent.classList.add('editor__content');
-        editorHtml.append(editorHtmlContent);
-        this.createContent(this.data, editorHtmlContent);
+        const editorHtmlContent = createElement('div', 'editor__content', '', editorHtml);
+        this.createHtmlContentTags(this.data, editorHtmlContent);
     }
 
-    private createContent(data: Array<string | string[]>, parentElement: HTMLElement): void {
+    private createHtmlContentTags(data: Array<string | string[]>, parentElement: HTMLElement): void {
         data.forEach((item: string | string[]) => {
             if (typeof item === 'string') {
                 createElement('div', 'editor__line', item, parentElement);
             } else {
                 const editorBlock = createElement('div', 'editor__block', '', parentElement);
-                this.createContent(item, editorBlock);
+                this.createHtmlContentTags(item, editorBlock);
             }
         });
     }
