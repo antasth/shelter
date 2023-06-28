110babaf1d854ff0ede7d657d6a02e03e1be8aa6
Anton Astashov <antasth@mail.ru>
2023-06-28 15:59:54 +0300

    refactor: class Submit using getElement function

--- rss-css-selectors/src/components/controller/listeners/submit.ts
+++ rss-css-selectors/src/components/controller/listeners/submit.ts
@@ -1,14 +1,15 @@
 import levels from '../../../data/levels';
+import { getElement } from '../../../functions/functions';
 
 class Submit {
     checkAnswer(level: number) {
-        const answer: HTMLInputElement | null = document.querySelector('.editor__input');
+        const answer: HTMLInputElement = getElement('.editor__input');
         if (answer) {
             return answer.value === levels[level].answer;
         }
     }
     clearInput() {
-        const answer: HTMLInputElement | null = document.querySelector('.editor__input');
+        const answer: HTMLInputElement = getElement('.editor__input');
         if (answer) {
             answer.value = '';
         }
