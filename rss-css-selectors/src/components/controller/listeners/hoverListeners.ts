30d3275af4c160249ebd04640c7948923bb97391
Anton Astashov <antasth@mail.ru>
2023-06-29 16:55:28 +0300

    refactor: HoverListeners class according to DRY

--- rss-css-selectors/src/components/controller/listeners/hoverListeners.ts
+++ rss-css-selectors/src/components/controller/listeners/hoverListeners.ts
@@ -9,39 +9,38 @@ class HoverListeners {
     }
     private addListener(parent: HTMLElement, child: HTMLElement): void {
         parent.addEventListener('mouseover', (event: Event) => {
-            if (event.target && event.target instanceof HTMLElement) {
-                const target = event.target;
-                const elementId = target.dataset.index ? target.dataset.index : target.parentElement?.dataset.index;
-                const [childElement] = [...child.children].filter((item) => {
-                    if (item instanceof HTMLElement && item.dataset.index === elementId) {
-                        return item;
-                    }
-                });
-                if (
-                    childElement?.classList.contains('editor__line') ||
-                    childElement?.classList.contains('editor__block')
-                ) {
-                    childElement?.classList.add('active-line');
-                } else childElement?.classList.add('active-element');
-            }
+            const childElement: HTMLElement = this.getChildElement(event, child);
+            this.toggleActiveClass('mouseover', childElement);
         });
         parent.addEventListener('mouseout', (event: Event) => {
-            if (event.target && event.target instanceof HTMLElement) {
-                const target = event.target;
-                const elementId = target.dataset.index ? target.dataset.index : target.parentElement?.dataset.index;
-                const [childElement] = [...child.children].filter((item) => {
-                    if (item instanceof HTMLElement && item.dataset.index === elementId) {
-                        return item;
-                    }
-                });
-                if (
-                    childElement?.classList.contains('editor__line') ||
-                    childElement?.classList.contains('editor__block')
-                ) {
-                    childElement?.classList.remove('active-line');
-                } else childElement?.classList.remove('active-element');
-            }
+            const childElement: HTMLElement = this.getChildElement(event, child);
+            this.toggleActiveClass('mouseout', childElement);
         });
+    }
+    private toggleActiveClass(eventType: string, element: HTMLElement): void {
+        if (eventType === 'mouseover') {
+            if (element?.classList.contains('editor__line') || element?.classList.contains('editor__block')) {
+                element?.classList.add('active-line');
+            } else element?.classList.add('active-element');
+        }
+        if (eventType === 'mouseout') {
+            if (element?.classList.contains('editor__line') || element?.classList.contains('editor__block')) {
+                element?.classList.remove('active-line');
+            } else element?.classList.remove('active-element');
+        }
+    }
+    private getChildElement(event: Event, child: HTMLElement): HTMLElement {
+        let childElement: Element | null = null;
+        if (event.target && event.target instanceof HTMLElement) {
+            const target: HTMLElement = event.target;
+            const elementId = target.dataset.index ? target.dataset.index : target.parentElement?.dataset.index;
+            [childElement] = [...child.children].filter((item) => {
+                if (item instanceof HTMLElement && item.dataset.index === elementId) {
+                    return item;
+                }
+            });
+        }
+        return childElement as HTMLElement;
     }
 }
 
