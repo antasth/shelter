2d4ef9caaefeed06581d4272190499ad4c7f0db1
Anton Astashov <antasth@mail.ru>
2023-07-05 20:05:37 +0300

    refactor: remove AppController class

--- rss-css-selectors/src/components/app/app.ts
+++ rss-css-selectors/src/components/app/app.ts
@@ -1,12 +1,10 @@
 import { getFromLocalStorage } from '../../functions/functions';
 import { GameData } from '../../interfaces/interfaces';
-import AppController from '../controller/appController';
 import Listeners from '../controller/listeners/buttonListeners';
 import AppView from '../view/appview';
 
 class App {
     private view: AppView;
-    private controller: AppController;
     private level: number;
     private storageData: GameData;
     private buttonListeners: Listeners;
@@ -15,7 +13,6 @@ class App {
         this.storageData = getFromLocalStorage();
         this.level = this.storageData ? this.storageData.currentLevel : 0;
         this.view = new AppView(this.level);
-        this.controller = new AppController(this.level);
         this.buttonListeners = new Listeners(this.level);
     }
 
@@ -25,7 +22,6 @@ class App {
         this.view.drawHelpButton();
         this.view.drawResetButton();
         this.view.drawEditor();
-        this.controller.addHoverListeners();
         this.buttonListeners.addButtonLeftListener();
         this.buttonListeners.addButtonRightListener();
         this.buttonListeners.addButtonResetListener();
