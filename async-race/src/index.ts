3d479aee9ddaf0e6c322187b0e81c0dfc7230847
Anton Astashov <antasth@mail.ru>
2023-07-11 13:06:15 +0300

    feat: add config files and settings

---
+++ async-race/src/index.ts
@@ -0,0 +1,8 @@
+import './index.html';
+import './styles/style.scss';
+
+const array: Array<string> = [];
+for (let index = 0; index < array.length; index += 1) {
+  const element = array[index];
+  console.log(element);
+}
---
+++ async-race/webpack.config.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    // this.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../dist'),
+        assetModuleFilename: 'assets/images/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
