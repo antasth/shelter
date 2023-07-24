dc344cd4184c5b3e003baf18ac6a0d7fb0d2c918
Anton Astashov <antasth@mail.ru>
2023-07-24 17:59:22 +0300

    refactor: add try catch to engine requests

--- async-race/src/api/engine.ts
+++ async-race/src/api/engine.ts
@@ -3,24 +3,36 @@ import { EngineStatus } from '../interfaces/enum';
 import { Engine, EngineDriveResponse } from '../interfaces/interfaces';
 
 export const startEngine = async (id: number): Promise<Engine> => {
-  const response = await fetch(`${ENGINE_PATH}?id=${id}&status=${EngineStatus.start}`, {
-    method: 'PATCH'
-  });
+  let result;
+  try {
+    const response = await fetch(`${ENGINE_PATH}?id=${id}&status=${EngineStatus.start}`, {
+      method: 'PATCH'
+    });
+    result = response.json();
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
+  return result;
+};
 
-  return response.json();
-};
 export const stopEngine = async (id: number): Promise<Engine> => {
-  const response = await fetch(`${ENGINE_PATH}?id=${id}&status=${EngineStatus.stop}`, {
-    method: 'PATCH'
-  });
-  return response.json();
+  let result;
+  try {
+    const response = await fetch(`${ENGINE_PATH}?id=${id}&status=${EngineStatus.stop}`, {
+      method: 'PATCH'
+    });
+    result = response.json();
+  } catch (error) {
+    if (error instanceof Error) console.log(error.message);
+  }
+  return result;
 };
+
 export const switchToDriveMode = async (id: number): Promise<EngineDriveResponse> => {
   const response = await fetch(`${ENGINE_PATH}?id=${id}&status=${EngineStatus.drive}`, {
     method: 'PATCH'
   });
   const result = await response.json();
   result.id = id;
-
   return result;
 };
